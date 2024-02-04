4343e096eb2d4da0ccb47516c0decfde1896da4f
Piyush289kumar <Piyushraikwar289@gmail.com>
2024-02-04 21:37:15 +0530

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,13 @@ module.exports = {
         "color-blob" : "#A427DF",
       }
     },
+    container:{
+      center:true,
+      padding:{
+        DEFAULT:'20px',
+        md:'50px'
+      }
+    }
   },
   plugins: [],
 }
